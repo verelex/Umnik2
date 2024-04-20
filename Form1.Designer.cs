790eab07e67448d3a2a0c6ea244c48f8bf77e7aa
verelex <sdg7771@mail.ru>
2024-04-21 00:11:38 +0500

    form name

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -177,7 +177,7 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Umnik2";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
